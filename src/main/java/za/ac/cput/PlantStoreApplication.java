c148b6e3e900491a61f8857c6789301275fe4327
Natheer13 <natheer.shade@gmail.com>
2024-05-17 01:18:06 +0200

    1st commit

---
+++ src/main/java/za/ac/cput/PlantStoreApplication.java
@@ -0,0 +1,13 @@
+package za.ac.cput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantStoreApplication.class, args);
+	}
+
+}
