0356eb19e1ca64acb8bf07c1adc10e10dbc879e3
Thabo <justicethabo21@gmail.com>
2024-05-17 19:57:10 +0200

    Supplier and Product classes added

---
+++ src/main/java/za/ac/cput/repository/SupplierRepository.java
@@ -0,0 +1,8 @@
+package za.ac.cput.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository {
+}
