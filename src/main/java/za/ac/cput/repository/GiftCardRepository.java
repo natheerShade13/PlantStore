71bf93e8c0c97587944db1de1813e5cdcb338c5c
Natheer13 <natheer.shade@gmail.com>
2024-05-18 14:24:57 +0200

    Updates.

--- src/main/java/za/ac/cput/repository/GiftCardRepository.java
+++ src/main/java/za/ac/cput/repository/GiftCardRepository.java
@@ -2,8 +2,8 @@ package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import za.ac.cput.domain.GiftCards;
+import za.ac.cput.domain.GiftCard;
 
 @Repository
-public interface GiftCardsRepository extends JpaRepository<GiftCards, Long> {
+public interface GiftCardRepository extends JpaRepository<GiftCard, Long> {
 }
