feb5d4f48a128b3c5e5fef4d73caa57040b0315c
Chadwin Kyle Fritz <100412900+ChadwinFritz@users.noreply.github.com>
2024-05-18 18:47:44 +0200

    Factory and FactoryTest

--- src/test/java/za/ac/cput/factory/EmployeeFactoryTest.java
+++ src/test/java/za/ac/cput/factory/EmployeeFactoryTest.java
@@ -1,26 +1,37 @@
 package za.ac.cput.factory;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.TestMethodOrder;
 import za.ac.cput.domain.Employee;
+import za.ac.cput.domain.Message;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class EmployeeFactoryTest {
 
+    private Employee employeeA;
+    private Employee employeeB;
+    private Employee employeeC;
+
     @Test
+    @Order(1)
     void buildEmployee() {
-        long employeeId = 123L; // Assuming employeeId is a long value
-        Employee employee = EmployeeFactory.buildEmployee(employeeId, "John Doe", "john@example.com", "123 Main St", "1234567890", "Manager", new ArrayList<>());
-        assertNotNull(employee);
+        employeeA = EmployeeFactory.buildEmployee(1L, "Jodi-Lee", "jodileerudolph@gmail.com", "123 Main St", "1234567890", "Manager", new ArrayList<Message>());
+        assertNotNull(employeeA);
+        System.out.println(employeeA);
     }
 
     @Test
-    void testBuildEmployee() {
-        Employee employee = EmployeeFactory.buildEmployee("Jane Doe", "jane@example.com", "456 Side St", "0987654321", "Assistant", new ArrayList<>());
-        assertNotNull(employee);
+    @Order(2)
+    void copyBuildEmployee() {
+        employeeC = EmployeeFactory.buildEmployee(2L, "Chadwin", "chadwinkyle2@gmail.com", "123 Main St", "9876543210", "Assistant", new ArrayList<Message>());
+        employeeB = new Employee.Builder().copy(employeeC).setRole("Senior Assistant").build();
+        assertNotNull(employeeB);
+        System.out.println(employeeB);
     }
 }
